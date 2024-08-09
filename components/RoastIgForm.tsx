238fb1e188267fe80a29154b1e076b18f9f15442
xnabil354 <xnbl354@gmail.com>
2024-08-09 13:39:15 +0700

    feat: add backend instagram api

--- components/RoastIgForm.tsx
+++ components/RoastIgForm.tsx
@@ -7,6 +7,7 @@ export default function RoastingForm() {
   const [model, setModel] = useState('geminiai');
   const [roasting, setRoasting] = useState('');
   const [loading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(true);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -27,6 +28,20 @@ export default function RoastingForm() {
 
   return (
     <div className="max-w-md mx-auto mt-10">
+        {showModal && (
+                <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
+                    <div className="bg-white p-6 rounded shadow-lg">
+                        <h2 className="text-2xl font-bold mb-4">Disclaimer</h2>
+                        <p className="mb-4">Hanya sekedar Hiburan!! Jangan dibawa Hati!</p>
+                        <button
+                            className="bg-blue-500 text-white px-4 py-2 rounded"
+                            onClick={() => setShowModal(false)}
+                        >
+                            I Understand
+                        </button>
+                    </div>
+                </div>
+            )}
       <form onSubmit={handleSubmit} className="space-y-4">
         <input
           type="text"
